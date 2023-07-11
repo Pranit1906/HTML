a156466339738a5da61440fb71155eec4dceab2c
Pranit <pranitdubal5@gmail.com>
2023-07-12 01:09:28 +0530

    HTML

---
+++ HTML_vedant/Client_Server_Architecture.js
@@ -0,0 +1,42 @@
+/*
+
+
+Client-Server Architecture.
+●      Server as its name suggests its duty is to serve the data according to the request.
+
+●      Client is a consumer of information, clients always make a request for the specific data to the server  
+       by typing the URL (request).
+
+●      If the server contains the requested information, then the server sends the information to the client.
+
+●      client-server architecture technically by relating with the real life example.
+
+
+
+
+
+ 
+
+
+1)   From the above image, customers who want to open a bank account , at the first stage, must know the address of the bank and for interacting with the bank they must have the knowledge of language.
+
+2)  Customer requests for the account opening at the bank and then the bank responds with an application form here application form is the set of rules which everyone has to follow .
+
+3)   Customers  fill the form and submit it to the bank and then the bank responds with account details. Here bank details are the key which is used by a person for future communication.
+
+4)  If a customer wants to change  his address  then request for an update giving the account details.then bank send the response and change the customer address.
+
+5)  so this is the general example for how client and server communicated.
+
+
+
+What is a client?
+
+client is responsible to send http requests for web pages to the server.
+
+What is a server?
+
+Server is the machine who serves the request made by the client.
+
+
+*/
